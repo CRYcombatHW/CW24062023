647a70948508757b4b887007a4784fe044d9fd5d
CRYcombat <koteika5063@gmail.com>
2023-06-24 12:57:35 +0300

    Basic Functional

--- CW24062023/Form1.Designer.cs
+++ CW24062023/Form1.Designer.cs
@@ -25,12 +25,78 @@
 		///  the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			button = new Button();
+			numericDay = new NumericUpDown();
+			numericMounth = new NumericUpDown();
+			numericYear = new NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)numericDay).BeginInit();
+			((System.ComponentModel.ISupportInitialize)numericMounth).BeginInit();
+			((System.ComponentModel.ISupportInitialize)numericYear).BeginInit();
+			SuspendLayout();
+			// 
+			// button
+			// 
+			button.Location = new Point(12, 93);
+			button.Name = "button";
+			button.Size = new Size(258, 23);
+			button.TabIndex = 1;
+			button.Text = "button";
+			button.UseVisualStyleBackColor = true;
+			button.Click += button_Click;
+			// 
+			// numericDay
+			// 
+			numericDay.Location = new Point(12, 12);
+			numericDay.Maximum = new decimal(new int[] { 31, 0, 0, 0 });
+			numericDay.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+			numericDay.Name = "numericDay";
+			numericDay.Size = new Size(82, 23);
+			numericDay.TabIndex = 2;
+			numericDay.Value = new decimal(new int[] { 24, 0, 0, 0 });
+			// 
+			// numericMounth
+			// 
+			numericMounth.Location = new Point(100, 12);
+			numericMounth.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
+			numericMounth.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+			numericMounth.Name = "numericMounth";
+			numericMounth.Size = new Size(82, 23);
+			numericMounth.TabIndex = 3;
+			numericMounth.Value = new decimal(new int[] { 6, 0, 0, 0 });
+			numericMounth.ValueChanged += numeric_UdpateDay;
+			// 
+			// numericYear
+			// 
+			numericYear.Location = new Point(188, 12);
+			numericYear.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+			numericYear.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+			numericYear.Name = "numericYear";
+			numericYear.Size = new Size(82, 23);
+			numericYear.TabIndex = 4;
+			numericYear.Value = new decimal(new int[] { 2023, 0, 0, 0 });
+			numericYear.ValueChanged += numeric_UdpateDay;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(284, 128);
+			Controls.Add(numericYear);
+			Controls.Add(numericMounth);
+			Controls.Add(numericDay);
+			Controls.Add(button);
+			Name = "Form1";
+			Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)numericDay).EndInit();
+			((System.ComponentModel.ISupportInitialize)numericMounth).EndInit();
+			((System.ComponentModel.ISupportInitialize)numericYear).EndInit();
+			ResumeLayout(false);
 		}
 
 		#endregion
+		private Button button;
+		private NumericUpDown numericDay;
+		private NumericUpDown numericMounth;
+		private NumericUpDown numericYear;
 	}
 }
--- CW24062023/Form1.cs
+++ CW24062023/Form1.cs
@@ -5,5 +5,18 @@ namespace CW24062023
 		public Form1() {
 			InitializeComponent();
 		}
+
+		private void button_Click(object sender, EventArgs e) {
+			MessageBox.Show(
+				$"Now is {new DateTime((int)numericYear.Value, (int)numericMounth.Value, (int)numericDay.Value).DayOfWeek}",
+				"Day of week",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Information
+			);
+		}
+
+		private void numeric_UdpateDay(object sender, EventArgs e) {
+			numericDay.Maximum = DateTime.DaysInMonth((int)numericYear.Value, (int)numericMounth.Value);
+		}
 	}
 }
